111c5ecf3c987eb2c002d1a221a754cc64124c2a
Nikita Zelenyak <Zelenyaknikita0927@gmail.com>
2022-07-29 12:20:10 +0800

    add

---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,38 @@
+import { Wrapper,Title,Label,Input,Item,Button,Text } from "./Contacts.styled"
+import{TiUserDelete} from'react-icons/ti'
+import PropTypes from 'prop-types'; 
+
+export const Contacts = ({ contacts, onDelete, onFilter, }) => {
+     
+    return (
+   
+        <Wrapper>
+          
+
+            <Title>Contacts</Title>
+            {contacts.length === 0 ? <Title>Add your first contact:)</Title> :   
+                <>
+           <Label htmlFor='find'>Find contacts by name</Label>
+              <Input id="find" type="text"  onChange={onFilter} />
+            <ul>
+                {contacts.map(contact => (
+                    <Item key={contact.id}>
+                        <Text>{contact.name}: {contact.number}</Text>
+                        <Button type="button" onClick={()=>onDelete(contact.id)}><TiUserDelete color="#763f33" size={22}></TiUserDelete></Button>
+                    </Item>
+                   
+                ))}
+
+            </ul></> }
+    </Wrapper>
+)
+
+   
+    }
+
+
+Contacts.prototype = {
+    contacts: PropTypes.arrayOf(PropTypes.object),
+    onDelete: PropTypes.func,
+    onFilter:PropTypes.func
+}
